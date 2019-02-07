82fbc7425ec4d1569d12bcdf886e18252d7428f9
Caleb Bassi <calebjbassi@gmail.com>
2019-02-06 19:23:56 -0800

    Add .travis.yml and reconfigure release process
    - travis-ci used to publish binaries to the GitHub releases tab on new release
    - Will also publish .deb and .rpm packages
    - Haven't gotten the checksums file to work yet
    - No longer using goreleaser to publish binaries
        - didn't support cgo cross compilation which made things difficult
    - Remove obsolete Makefile

---
+++ ci/script.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+go build -o ${NAME}
+
+if [[ ${GOARCH} == "arm64" ]]; then
+    FILE=${NAME}_${TRAVIS_TAG}_${GOOS}_arm8
+else
+    FILE=${NAME}_${TRAVIS_TAG}_${GOOS}_${GOARCH}${GOARM}
+fi
+
+tar -czf dist/${FILE}.tgz ${NAME}
+
+if [[ ${GOOS} == "linux" && ${GOARCH} == "amd64" ]]; then
+    go get github.com/goreleaser/nfpm
+    export VERSION=$(go run main.go -v)
+    nfpm pkg --config ci/nfpm.yml --target dist/${NAME}.deb
+    nfpm pkg --config ci/nfpm.yml --target dist/${NAME}.rpm
+fi
