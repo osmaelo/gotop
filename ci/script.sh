c7d8f7c4c4a1619c9b894a0385fa25f25c31def6
Caleb Bassi <calebjbassi@gmail.com>
2019-02-06 22:02:38 -0800

    travis: fix ci/script.sh

--- ci/script.sh
+++ ci/script.sh
@@ -2,8 +2,10 @@
 
 go build -o ${NAME}
 
+mkdir -p dist
+
 if [[ ${GOARCH} == "arm64" ]]; then
-    FILE=${NAME}_${TRAVIS_TAG}_${GOOS}_arm8
+    FILE=${NAME}_${TRAVIS_BRANCH}_${GOOS}_arm8
 else
     FILE=${NAME}_${TRAVIS_BRANCH}_${GOOS}_${GOARCH}${GOARM}
 fi
@@ -13,6 +15,6 @@ tar -czf dist/${FILE}.tgz ${NAME}
 if [[ ${GOOS} == "linux" && ${GOARCH} == "amd64" ]]; then
     go get github.com/goreleaser/nfpm
     export VERSION=$(go run main.go -v)
-    nfpm pkg --config ci/nfpm.yml --target dist/${NAME}.deb
-    nfpm pkg --config ci/nfpm.yml --target dist/${NAME}.rpm
+    nfpm pkg --config ci/nfpm.yml --target dist/${FILE}.deb
+    nfpm pkg --config ci/nfpm.yml --target dist/${FILE}.rpm
 fi
